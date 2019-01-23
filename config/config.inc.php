31fc3a4e4156bdb63a0b3725731c9df7a8fe099d
Thibaut <t.delplanque92@gmail.com>
2019-01-23 16:43:10 +0100

    Création du projet shoppin-bag-project

---
+++ config/config.inc.php
@@ -0,0 +1,10 @@
+
+<?php
+  global $CFG;
+  $CFG = new stdClass();
+  $CFG->srvName = 'localhost';
+  $CFG->port = '3306';
+  $CFG->login = 'root';
+  $CFG->password = '';
+  $CFG->dbName = 'gestRDV';
+?>
---
+++ create_shoppping_bag_bd.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 23 jan. 2019 à 15:16
+-- Version du serveur :  5.7.23
+-- Version de PHP :  7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `shopping_bag_bd`
+--
+CREATE DATABASE IF NOT EXISTS `shopping_bag_bd` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `shopping_bag_bd`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `adresse`
+--
+
+DROP TABLE IF EXISTS `adresse`;
+CREATE TABLE IF NOT EXISTS `adresse` (
+  `id_adresse` int(11) NOT NULL AUTO_INCREMENT,
+  `adresse` varchar(50) NOT NULL,
+  `complement_adresse` varchar(50) NOT NULL,
+  `code_postal` int(11) NOT NULL,
+  `ville` char(5) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`id_adresse`),
+  UNIQUE KEY `adresse_utilisateur_ak` (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+DROP TABLE IF EXISTS `categorie`;
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id_categorie` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_categorie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id_commande` int(11) NOT NULL AUTO_INCREMENT,
+  `date_commande` date NOT NULL,
+  `statut` varchar(50) NOT NULL,
+  `session` varchar(100) NOT NULL,
+  `total` float NOT NULL,
+  `type_paiement` varchar(100) NOT NULL,
+  `utilisateur_enregistre` varchar(100) NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`id_commande`),
+  KEY `commande_utilisateur_fk` (`id_utilisateur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande_produit`
+--
+
+DROP TABLE IF EXISTS `commande_produit`;
+CREATE TABLE IF NOT EXISTS `commande_produit` (
+  `id_commande` int(11) NOT NULL,
+  `id_produit` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`id_commande`,`id_produit`),
+  KEY `contient_produit_fk` (`id_produit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+DROP TABLE IF EXISTS `produit`;
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id_produit` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `prix` float NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `id_categorie` int(11) NOT NULL,
+  PRIMARY KEY (`id_produit`),
+  KEY `produit_categorie_fk` (`id_categorie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `identifiant` varchar(50) NOT NULL,
+  `mot_de_Passe` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `telephone` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `admin` varchar(1) NOT NULL,
+  `id_adresse` int(11) NOT NULL,
+  PRIMARY KEY (`id_utilisateur`),
+  UNIQUE KEY `utilisateur_adresse_ak` (`id_adresse`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `adresse`
+--
+ALTER TABLE `adresse`
+  ADD CONSTRAINT `adresse_utilisateur_fk` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `commande_utilisateur_fk` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`);
+
+--
+-- Contraintes pour la table `commande_produit`
+--
+ALTER TABLE `commande_produit`
+  ADD CONSTRAINT `contient_commande_fk` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id_commande`),
+  ADD CONSTRAINT `contient_produit_fk` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id_produit`);
+
+--
+-- Contraintes pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD CONSTRAINT `produit_categorie_fk` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`);
+
+--
+-- Contraintes pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD CONSTRAINT `utilisateur_adresse_fk` FOREIGN KEY (`id_adresse`) REFERENCES `adresse` (`id_adresse`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
