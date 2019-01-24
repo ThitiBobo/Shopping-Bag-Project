7ca8e5c953d3e39162dafdfd4f7cfdfb074b23bb
Thibaut <t.delplanque92@gmail.com>
2019-01-24 12:34:53 +0100

    Ajout de testAdresseManager, ca marche !

---
+++ test/testAdresseManageer.php
@@ -0,0 +1,31 @@
+<?php
+
+    use models\Model;
+    use models\Adresse;
+    use DAO\AdresseManager;
+    use persistent\Bdd;
+
+    include_once('../persistent/Bdd.php');
+    include_once('../models/Adresse.php');
+    include_once('../DAO/AdresseManager.php');
+
+
+    $obj = new Adresse;
+    $obj->adresse = "65 rue du test";
+    $obj->complementAdresse = "deuxième étage";
+    $obj->codePostal = "69008";
+    $obj->ville = "Lyon";
+
+    $manager = new AdresseManager(Bdd::getInstance());
+
+    $manager->add($obj);
+    $manager->add($obj);
+    print_r($manager->get($obj->id));
+
+    $liste = $manager->getList();
+    print_r($liste);
+
+    foreach ($liste as $var) {
+      $manager->delete($var);
+    }
+ ?>
